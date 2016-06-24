ba8903bd2f7583c38346154e012dd48d2cb3624e
vardin <vardin@starcom>
2016-06-24 16:06:00 +0900

    e

---
+++ src/main/java/producer/producer.java
@@ -0,0 +1,20 @@
+package producer;
+
+
+import java.util.Properties;
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+public class producer {
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put("metadata.broker.list", "kafka1:9092,kafka2:9092,kafka3:9092");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        ProducerConfig producerConfig = new ProducerConfig(props);
+        Producer<String, String> producer = new Producer<String, String>(producerConfig);
+        KeyedMessage<String, String> message = new KeyedMessage<String, String>("", "Hello, World!");  
+        producer.send(message);
+        producer.close();
+    }
+}
